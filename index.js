05daeb722a5e027f1939e5f12a0ee28f147bb9e0
jonathan bola <bolajonathan@yahoo.com>
2022-01-31 09:02:20 -0600

    commit tree 76b5879f48e75a21fce8bfab720721ac93ce172d

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() +  str.slice(1)
+
+
+  }
+  static sanitize(str){
+    str = JSON.stringify(str).replace(/\W/g, '')
+    
+
+
+  }
+  
+  static titleize(str){
+
+
+  }
+  
+
+
 }
